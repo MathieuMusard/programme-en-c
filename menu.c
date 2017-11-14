3ef1b7a7b4dd1ca4e3ce36430ca1199efeecbc5f
Geoffroy Yann <yannyann418@gmail.com>
2017-11-14 08:57:18 +0100

    début de menu

--- menu.c
+++ menu.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "menu.h"
+
+void afficherMenu(){
+    int choix;
+    FILE *fichier
+    printf("******* Bienvenue dans l'electrocardiofrequenceOmaitre *******\n\n");
+    printf("Que voulez vous faire ?\n\n");
+    printf("1.Afficher les donnees dans l'ordre du fichier\n")
+    printf("2.Afficher les donnees en ordre croissant");
+    printf("3.Afficher les donnes dans un ordre decroissant");
+    printf("4.Rechercher les données pour un temps particulier");
+    printf("5.Afficher le nombre de lignes de données en mémoire");
+    printf("6.Rechercher et afficher les min / max de pouls");
+    fichier = fopen("")
+    switch (choix)
+    {
+    case 0:
+        return 0;
+        break
+    case 1:
+
+
+
+
+
+
+    }
+
+    return 0;
+}
